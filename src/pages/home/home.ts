8fec2823a5cecae4bc7625ea76890ea8e0869b99
Andrew Shell <andrew@andrewshell.org>
2018-04-30 14:59:56 -0500

    Upload button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('file') file;
 
-  }
+  constructor(
+    private http: HttpClient
+  ) {}
+
+    upload() {
+        let event = new MouseEvent('click', {bubbles: true});
+        this.file.nativeElement.dispatchEvent(event);
+    }
+
+    onFileChange(event) {
+        if (event.target.files && event.target.files.length > 0) {
+            let file = event.target.files[0];
+            let input = new FormData();
+            input.append('image', file);
+
+            this.http.post('/upload.php', input)
+              .toPromise()
+              .then((res) => {
+                console.dir(res);
+              })
+              .catch((err) => {
+                console.dir(err);
+              });
+        }
+    }
 
 }
