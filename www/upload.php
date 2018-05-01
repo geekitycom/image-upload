dfd0e0f083f8f3fa7e4e32952a29ea51551144a4
Andrew Shell <andrew@andrewshell.org>
2018-05-01 14:46:09 -0500

    Tweaking headers for upload.php

--- www/upload.php
+++ www/upload.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo json_encode($_FILES);
